678bfc39d113f648c56967dc883fee2fb429fc49
Ezio <assassinste@gmail.com>
2020-02-14 17:44:36 +0800

    Different types that will be used in this book

---
+++ level1_acquaintance/list1.3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+Datatypes that used in this book
+  For the scalar types, here you are:
+    double         %e, %f, %g, %a
+    bool           %d (0, 1)
+    char           %c
+    char const*    %s
+    size_t         %zu, %zx      https://stackoverflow.com/questions/2550774/what-is-size-t-in-c
+    signed         %d            == signed int
+    unsigned       %u, %x        == unsigned int
+    ptrdiff_t      %td           https://en.cppreference.com/w/c/types/ptrdiff_t
+    void*          %p            https://www.learncpp.com/cpp-tutorial/613-void-pointers/
+    unsigned char  %hhu, %0xhhx  https://www.geeksforgeeks.org/unsigned-char-in-c-with-examples/
+*/
+int main(void)
+{
+    // pass
+}
